d18e1afe8e39a059f279099f6637d0b02bd8ba46
gustavoalmeidaney <gustavo.emanoel.almeida@escola.pr.gov.br>
2025-10-02 11:49:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(Button =>{
-   Button.addEventListener('click', function(){
-   const atual = document.querySelector('.ativo');
-   const proximopasso = 'passo-' + this.getattribute('data-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-   atual.classList.remove('ativo');
-   document.getElementById(proximopasso).classList.add('ativo');
-   })
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
 })
